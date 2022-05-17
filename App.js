9083bec763d0c847484335204d6116a95c0a8f9d
Sai Rashwant <sairashwantno1@gmail.com>
2022-05-17 11:21:21 +0530

    first

---
+++ App.js
@@ -0,0 +1,21 @@
+import Stopwatch from './components/stopwatch';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Stopwatch/>
+      
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'black',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
